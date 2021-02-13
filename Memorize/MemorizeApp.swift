0610cbfc4c9b8a62c5e100a9de0cf8cd6fef68ff
Bruno Henrique de Borba <brunohenriqueborba@gmail.com>
2021-02-13 15:45:17 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bruno Henrique de Borba on 13/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
